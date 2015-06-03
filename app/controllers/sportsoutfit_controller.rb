4b5c509b7f5f6cd718b8a9391f2a677c1668b7a8
abdul basit <abdulbasit558@yahoo.com>
2015-06-03 18:17:28 +0000

    make a view controller

--- app/controllers/sportsoutfit_controller.rb
+++ app/controllers/sportsoutfit_controller.rb
@@ -1,6 +1,15 @@
 class SportsoutfitController < ApplicationController
     def index
+    
+    end
+    
+    def show
+      
+    end
+    
+    def new
+    @user = User.new     
     end
 
-
 end
+ 
--- db/migrate/20150603075639_delete_logo.rb
+++
@@ -1,4 +0,0 @@
-class DeleteLogo < ActiveRecord::Migration
-  def change
-  end
-end
